fdab28ea2bd7a359d1d6c82747c26d4e7c0a476a
jamesbeedy <jamesbeedy@gmail.com>
2017-12-06 11:18:05 -0800

    sync with upstream charms.reactive

--- requires.py
+++ requires.py
@@ -1,24 +1,24 @@
-from charms.reactive import when_any, when_not
+from charms.reactive import when, when_not, when_any
 from charms.reactive import set_flag, clear_flag
 from charms.reactive import Endpoint
 
 
 class ElasticsearchRequires(Endpoint):
+
     @when_any('endpoint.{endpoint_name}.changed.host',
               'endpoint.{endpoint_name}.changed.port',
               'endpoint.{endpoint_name}.changed.cluster_name')
-    def relation_state_modified(self):
-        # Detect changes to the host or port field on any remote unit
-        # and translate that into the host-port flag. Then, clear the
-        # changed field flags so that we can detect further changes.
-        set_flag(self.flag('endpoint.{endpoint_name}.host-port'))
-        clear_flag(self.flag('endpoint.{endpoint_name}.changed.host'))
-        clear_flag(self.flag('endpoint.{endpoint_name}.changed.port'))
-        clear_flag(self.flag('endpoint.{endpoint_name}.changed.cluster_name'))
+    def changed(self):
+        if any(unit.received['port'] for unit in self.all_units):
+            set_flag('endpoint.{endpoint_name}.available')
+        clear_flag('endpoint.{endpoint_name}.changed.host')
+        clear_flag('endpoint.{endpoint_name}.changed.port')
+        clear_flag('endpoint.{endpoint_name}.changed.cluster_name')
 
     @when_not('endpoint.{endpoint_name}.joined')
     def broken(self):
-        clear_flag(self.flag('endpoint.{endpoint_name}.host-port'))
+        clear_flag('endpoint.{endpoint_name}.available')
+
 
     def relation_data(self):
         """
@@ -56,5 +56,3 @@ class ElasticsearchRequires(Endpoint):
                     'unit_name': unit.unit_name,
                 })
         return units_data
-
-
